76ae77f92d07bdc4b43ca74cb4cb4893fee470d2
mjftw <mjftwebster@gmail.com>
2015-01-08 17:22:42 +0000

    updated CORDIC

--- ELEC3221/cordic_multi_block_test.sv
+++ ELEC3221/cordic_multi_block_test.sv
@@ -1,37 +1,37 @@
 module cordic_multi_blk_test;
-	logic signed [15:0] x_in;
-	logic signed [15:0] y_in;
-	logic signed [15:0] z_in;
-	logic reset;
-	logic clk;
-	logic start;
-	logic signed [15:0] x_out;
-	logic signed [15:0] y_out;
-	logic signed [15:0] z_out;
-	logic data_out_rot;
+	logic signed [15:0] x_in, y_in, z_in, xprime, yprime;
+	logic reset, clk, start, data_out_rot;
 	
 	wire signed [15:0] x_regs [15:1];
 	wire signed [15:0] y_regs [15:1];
 	wire signed [15:0] z_regs [15:1];
 	wire signed valid_flags [15:1];
 	
+	wire signed [15:0] xprime_unscaled, yprime_unscaled;
+	
 	//wire z_out [15:0];
 	
 	
-	cordic_blk #(0) blk_0(.x_in(x), .y_in(y), .z_in(theta), .x_out(x_regs[1]), .y_out(y_regs[1]), .z_out(z_regs[1]),
+	cordic_blk #(0) blk_0(.x_in(x_in), .y_in(y_in), .z_in(z_in), .x_out(x_regs[1]), .y_out(y_regs[1]), .z_out(z_regs[1]),
 	.reset(reset), .clk(clk), .start(start), .valid_in(start), .valid_out(valid_flags[1]));	
 	
 	genvar i;
 	generate
-		for(i=2; i<15; i++)
+		for(i=1; i<15; i++)
 		begin
-			cordic_blk #(i-1) blk_1(.x_in(x_regs[i-1]), .y_in(y_regs[i]), .z_in(z_regs[i-1]), .x_out(x_regs[i]), .y_out(y_regs[i]), .z_out(z_regs[i]),
-			.reset(reset), .clk(clk), .start(start), .valid_in(valid_flags[i-1]), .valid_out(valid_flags[i]));
+			cordic_blk #(i) blk_i(.x_in(x_regs[i]), .y_in(y_regs[i]), .z_in(z_regs[i]), .x_out(x_regs[i+1]), .y_out(y_regs[i+1]), .z_out(z_regs[i+1]),
+			.reset(reset), .clk(clk), .start(start), .valid_in(valid_flags[i]), .valid_out(valid_flags[i+1]));
 		end
 	endgenerate
 	
-	cordic_blk #(15) blk_15(.x_in(x_regs[15]), .y_in(y_regs[15]), .z_in(z_regs[15]), .x_out(xprime), .y_out(yprime)/*, .z_out(z_out)*/,
+	cordic_blk #(15) blk_15(.x_in(x_regs[15]), .y_in(y_regs[15]), .z_in(z_regs[15]), .x_out(xprime_unscaled), .y_out(yprime_unscaled)/*, .z_out(z_out)*/,
 	.reset(reset), .clk(clk), .start(start), .valid_in(valid_flags[15]), .valid_out(data_out_rot));
+	
+	always_comb
+	begin
+		xprime = xprime_unscaled * 1.76;
+		yprime = yprime_unscaled * 1.76;
+	end
 	
 	initial //clock generation
 	begin
